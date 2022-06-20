d45d600c986549d2883ccd15b8a10dd26b6ef33a
AACSnielsen <snielsen@aac-us.com>
2022-06-20 18:40:01 -0400

    Add project files.

---
+++ AAC_CRUploader/frmCrUpload.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UploadCRFile
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCRFile = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.btnUpload = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cboMap = New System.Windows.Forms.ComboBox()
+        Me.dbMap = New System.Windows.Forms.DataGridView()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.dbMapz = New System.Windows.Forms.DataGridView()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.gvData = New System.Windows.Forms.DataGridView()
+        Me.chkNoUpdate = New System.Windows.Forms.CheckBox()
+        CType(Me.dbMap, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dbMapz, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.gvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtCRFile
+        '
+        Me.txtCRFile.Location = New System.Drawing.Point(107, 297)
+        Me.txtCRFile.Name = "txtCRFile"
+        Me.txtCRFile.Size = New System.Drawing.Size(472, 20)
+        Me.txtCRFile.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 300)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Cash Receipt File"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOpen.Location = New System.Drawing.Point(585, 298)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(21, 20)
+        Me.btnOpen.TabIndex = 2
+        Me.btnOpen.Text = "..."
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'btnUpload
+        '
+        Me.btnUpload.Location = New System.Drawing.Point(464, 539)
+        Me.btnUpload.Name = "btnUpload"
+        Me.btnUpload.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpload.TabIndex = 3
+        Me.btnUpload.Text = "Upload"
+        Me.btnUpload.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(545, 539)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 4
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(105, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "File Layout Template"
+        '
+        'cboMap
+        '
+        Me.cboMap.FormattingEnabled = True
+        Me.cboMap.Location = New System.Drawing.Point(120, 5)
+        Me.cboMap.Name = "cboMap"
+        Me.cboMap.Size = New System.Drawing.Size(471, 21)
+        Me.cboMap.TabIndex = 7
+        '
+        'dbMap
+        '
+        Me.dbMap.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dbMap.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dbMap.Location = New System.Drawing.Point(12, 133)
+        Me.dbMap.Name = "dbMap"
+        Me.dbMap.ReadOnly = True
+        Me.dbMap.Size = New System.Drawing.Size(624, 147)
+        Me.dbMap.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Data Mappings"
+        '
+        'dbMapz
+        '
+        Me.dbMapz.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dbMapz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dbMapz.Location = New System.Drawing.Point(12, 68)
+        Me.dbMapz.Name = "dbMapz"
+        Me.dbMapz.ReadOnly = True
+        Me.dbMapz.Size = New System.Drawing.Size(624, 46)
+        Me.dbMapz.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(13, 52)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Map Header"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(11, 333)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(71, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Data Preview"
+        '
+        'gvData
+        '
+        Me.gvData.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.gvData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.gvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.gvData.Location = New System.Drawing.Point(11, 348)
+        Me.gvData.Name = "gvData"
+        Me.gvData.Size = New System.Drawing.Size(624, 185)
+        Me.gvData.TabIndex = 13
+        '
+        'chkNoUpdate
+        '
+        Me.chkNoUpdate.AutoSize = True
+        Me.chkNoUpdate.Location = New System.Drawing.Point(11, 539)
+        Me.chkNoUpdate.Name = "chkNoUpdate"
+        Me.chkNoUpdate.Size = New System.Drawing.Size(107, 17)
+        Me.chkNoUpdate.TabIndex = 14
+        Me.chkNoUpdate.Text = "No SQL Updates"
+        Me.chkNoUpdate.UseVisualStyleBackColor = True
+        '
+        'UploadCRFile
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(676, 577)
+        Me.Controls.Add(Me.chkNoUpdate)
+        Me.Controls.Add(Me.gvData)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.dbMapz)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.dbMap)
+        Me.Controls.Add(Me.cboMap)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnUpload)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtCRFile)
+        Me.Name = "UploadCRFile"
+        Me.Text = "Upload Cash Receipt File"
+        CType(Me.dbMap, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dbMapz, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.gvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtCRFile As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents btnUpload As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cboMap As ComboBox
+    Friend WithEvents dbMap As DataGridView
+    Friend WithEvents Label3 As Label
+    Friend WithEvents dbMapz As DataGridView
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents gvData As DataGridView
+    Friend WithEvents chkNoUpdate As CheckBox
+End Class
---
+++ AAC_CRUploader/frmCrUpload.vb
@@ -0,0 +1,223 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.OleDb
+Imports System.IO
+
+
+Public Class UploadCRFile
+    Protected Friend FormLoad As Boolean
+    Protected Friend ReceiptId As String
+    Protected Friend gdtMap As DataTable
+    Protected Friend gImportNum As Integer
+
+
+
+    Private Sub UploadCRFile_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FormLoad = True
+        Dim lServer As String = "SDN-ENVY-2020\SDN_HPENVY"
+        Dim lDatabase As String = "TestDB"
+        Dim lSqlConnection As New SqlClient.SqlConnection
+        Dim lConnectionString As String
+        btnOpen.Enabled = False
+        txtCRFile.Enabled = False
+
+        lConnectionString = "Server=" & lServer & "; Database=" & lDatabase & ";Integrated Security=SSPI;"
+        lSqlConnection.ConnectionString = lConnectionString
+        lSqlConnection.Open()
+        Dim lCmdText As String = "Select Mapcode, MapDesc, FileMask, FileType from _aac_CRMAPZ where inactive <> 'Y'"
+
+        Dim ldaMaps As New SqlDataAdapter(lCmdText, lSqlConnection)
+        Dim ldtMaps As New DataTable
+        ldaMaps.Fill(ldtMaps)
+
+        With cboMap
+            .DataSource = ldtMaps
+            .DisplayMember = "MapDesc"
+            .ValueMember = "MapCode"
+        End With
+
+        lSqlConnection.Close()
+        FormLoad = False
+    End Sub
+
+
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        OpenFileDialog1.ShowDialog()
+        txtCRFile.Text = OpenFileDialog1.FileName
+    End Sub
+
+
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub cboMap_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboMap.SelectedIndexChanged
+        If Not FormLoad Then
+
+
+            Dim lServer As String = "SDN-ENVY-2020\SDN_HPENVY"
+            Dim lDatabase As String = "TestDB"
+            Dim lSqlConnection As New SqlClient.SqlConnection
+            Dim lConnectionString As String
+            lConnectionString = "Server=" & lServer & "; Database=" & lDatabase & ";Integrated Security=SSPI;"
+            lSqlConnection.ConnectionString = lConnectionString
+            lSqlConnection.Open()
+            Dim lCmdText As String = ""
+            'Endsure sequenece exists
+            lCmdText = "if not exists (select * from sys.sequences where schema_name(schema_id) = 'sequence' and name = 'import_num') " &
+                        "CREATE SEQUENCE [Sequence].[Import_Num]  START WITH 1"
+            Dim lcmd As New SqlCommand(lCmdText, lSqlConnection)
+            lcmd.ExecuteNonQuery()
+
+            lCmdText = "Select Mapcode, ApplicationType, SourceColumnLabel, TargetColumn, DataType from _aac_CRMAP where mapcode = '" & cboMap.SelectedValue & "' order by applicationType DESC;" &
+                       "Select MapCode, MapDesc, FileMask, FileType, ReceiptID from _aac_crmapz where mapcode = '" & cboMap.SelectedValue & "';" &
+                       "Select next value for sequence.import_num as Import_Num from _aac_crmap where mapcode = '" & cboMap.SelectedValue & "' and sourcecolumnlabel = '%ImportNum%';"
+            Dim ldaMap As New SqlDataAdapter(lCmdText, lSqlConnection)
+            'Dim ldtMap As New DataTable
+            Dim ldsMap = New DataSet
+            ldaMap.Fill(ldsMap)
+            'ldaMap.Fill(ldtMap)
+
+            gdtMap = ldsMap.Tables(0)
+            dbMap.DataSource = gdtMap 'ldsMap.Tables(0)
+
+            dbMapz.DataSource = ldsMap.Tables(1)
+            ReceiptId = ldsMap.Tables(1).Rows(0)("ReceiptID") 'Get identifier for receipt object
+            OpenFileDialog1.Filter = "CR IMport Files|" & ldsMap.Tables(1).Rows(0)("FileMask") 'Get identifier for receipt object
+
+            If ldsMap.Tables(2).Rows.Count > 0 Then
+                gImportNum = ldsMap.Tables(2).Rows(0)(0)
+            End If
+
+
+
+            btnOpen.Enabled = True
+            txtCRFile.Enabled = True
+
+
+            'With dbMap
+            ' .DataSource = ldtMap
+            ' End With
+        End If
+    End Sub
+
+    Private Sub btnUpload_Click(sender As Object, e As EventArgs) Handles btnUpload.Click
+        ' ldtMap hold the current mapping spec.  This includes multiple ApplicationType mappings.
+        ' Application types include, among others, 'R'eceipts Headers, 'A'pplications to BIlls
+        '  Get distinct types from mapping,  then for each type, get disctinct values
+        Dim lcmd As New SqlCommand
+        Dim lServer As String = "SDN-ENVY-2020\SDN_HPENVY"
+        Dim lDatabase As String = "TestDB"
+        Dim lSqlConnection As New SqlClient.SqlConnection
+        Dim lConnectionString As String
+        If chkNoUpdate.Checked = CheckState.Unchecked Then
+
+            lConnectionString = "Server=" & lServer & "; Database=" & lDatabase & ";Integrated Security=SSPI;"
+            lSqlConnection.ConnectionString = lConnectionString
+            lSqlConnection.Open()
+            lcmd.Connection = lSqlConnection
+
+
+        End If
+
+        'Open source data table
+        Dim dtCRData As New DataTable
+        Dim strCRSelect As String = "select * from [" & Path.GetFileName(OpenFileDialog1.FileName) & "]"
+        Dim csvConnection = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Path.GetDirectoryName(OpenFileDialog1.FileName) & ";Extended Properties=""text;HDR=Yes;FMT=Delimited"";"
+        Using csvDataAdapter As New OleDbDataAdapter(strCRSelect, csvConnection)
+            csvDataAdapter.Fill(dtCRData)
+        End Using
+
+
+        Dim distinctTypes As DataTable = gdtMap.DefaultView.ToTable(True, "ApplicationType")
+        For TypeIX = 0 To distinctTypes.Rows.Count - 1
+            Debug.Print("Processing Type = " & distinctTypes.Rows(TypeIX)(0).ToString)
+            'Get distinct mapping rows for select map and type
+            Dim TypeFilter As String = "ApplicationType = '" & distinctTypes.Rows(TypeIX)(0).ToString & "' and DataType = 'L'"
+            Dim ThisTypeRows As DataRow() = gdtMap.Select(TypeFilter)
+            Dim dtThisType As DataTable = gdtMap.Clone()
+            dtThisType = ThisTypeRows.CopyToDataTable
+            ' At this point dtThisType should have the mapping for the iterative data type (R, B, ...)
+            ' Process each row in the dtThistype mapping against all (distinct) input rows to write to database table
+            '  First set column list to select:
+            Dim ColumnList As New List(Of String)
+            For ColIX = 0 To dtThisType.Rows.Count - 1
+                ColumnList.Add(dtThisType.Rows(ColIX)("SourceColumnLabel"))
+            Next ColIX
+
+            'Update the map to include all columns
+            TypeFilter = "ApplicationType = '" & distinctTypes.Rows(TypeIX)(0).ToString & "' and DataType <> ''"
+            ThisTypeRows = gdtMap.Select(TypeFilter)
+            dtThisType = ThisTypeRows.CopyToDataTable
+            Dim txtPreamble As String = "insert into _aac_CR_LOAD ("
+            Dim txtValues As String = ""
+            For ColIX = 0 To dtThisType.Rows.Count - 1
+                txtPreamble &= (dtThisType.Rows(ColIX)("TargetColumn")) + ", "
+            Next ColIX
+            txtPreamble = txtPreamble.Substring(0, txtPreamble.Length - 2) & ")" & vbCrLf
+
+            'Now select the distinct values for the indiacted columns
+            Dim ReceiptLineSeq As Integer = 0
+            Dim CurrReceiptID As Integer = -1
+            Dim distinctRcpt As DataTable = dtCRData.DefaultView.ToTable(True, ColumnList.ToArray())
+
+            For rcptix = 0 To distinctRcpt.Rows.Count - 1
+                If CurrReceiptID <> distinctRcpt.Rows(rcptix)(ReceiptId) Then
+                    ReceiptLineSeq = 0
+                    CurrReceiptID = distinctRcpt.Rows(rcptix)(ReceiptId)
+                End If
+                ReceiptLineSeq += 1
+                txtValues = "Values ("
+                For colix = 0 To dtThisType.Rows.Count - 1
+                    Select Case dtThisType.Rows(colix)("DataType")
+                        Case "K" ' C(K)onstant
+                            txtValues &= "'" & (dtThisType.Rows(colix)("SourceColumnLabel")) & "', "
+                        Case "R" 'Replacement Variable
+                            Select Case (dtThisType.Rows(colix)("SourceColumnLabel"))
+                                Case "%FileName%"
+                                    txtValues &= "'" & OpenFileDialog1.FileName & "', "
+                                Case "%ImportNum%"
+                                    txtValues &= gImportNum.tostring & ", "
+                                Case "%Time%"
+                                    txtValues &= "getdate(), "
+                                Case "%User%"
+                                    txtValues &= "Suser_Sname(), "
+                                Case "%TranLine%"
+                                    txtValues &= ReceiptLineSeq.ToString + ", "
+                                Case Else
+                                    txtValues &= "'" & dtThisType.Rows(colix)("SourceColumnLabel") & "', "
+                            End Select
+                        Case Else
+                            txtValues &= "'" & distinctRcpt.Rows(rcptix)(dtThisType.Rows(colix)("SourceColumnLabel")) & "', "
+                    End Select
+
+                Next colix
+                txtValues = txtValues.Substring(0, txtValues.Length - 2) & ")"
+                Debug.Print(txtPreamble & txtValues)
+                If chkNoUpdate.Checked = CheckState.Unchecked Then
+                    lcmd.CommandText = txtPreamble & txtValues
+                    lcmd.ExecuteNonQuery()
+                End If
+            Next rcptix
+
+        Next TypeIX
+
+        'Dim DistinctReceipts = From row In dssample.Tables(0).AsEnumerable()
+        ' Select Case row.Field(Of String)(dssample.Tables(0))
+
+    End Sub
+
+    Private Sub txtCRFile_TextChanged(sender As Object, e As EventArgs) Handles txtCRFile.TextChanged
+        Dim dssample As New DataSet
+        Dim CSVSelect As String = "select * from [" & Path.GetFileName(OpenFileDialog1.FileName) & "]"
+        Dim CnStr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Path.GetDirectoryName(OpenFileDialog1.FileName) & ";Extended Properties=""text;HDR=Yes;FMT=Delimited"";"
+
+        Using Adp As New OleDbDataAdapter(CSVSelect, CnStr)
+            Adp.Fill(dssample)
+        End Using
+
+        gvData.DataSource = dssample.Tables(0)
+    End Sub
+
+
+End Class
